6769ebc09d16267d44169455af185711c8eaa996
veys <and.veys@gmail.com>
2021-05-16 21:26:47 +0300

    homework_#3

--- hw.cpp
+++ hw.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include <locale>				//для setlocale
-int main()
+int main() 
 {
 	setlocale(LC_CTYPE, "Russian");		//для вывода кирилицей  
-	std::cout << "Привет, Мир!" << std::endl;
+	int a = -1;
+	int i;
+	while (a < 2)
+	{
+		printf("Введите число больше 1: ");
+		scanf("%d", &a);
+	}
+	for(i=2; i<a; i++)	
+	{
+		if(a%i == 0)
+			break;		
+	}
+	printf("Чиcло %d является %s", a, (a==i) ? "простым" : "составным");	
 	return 0;
 }
