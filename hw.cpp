3a4e412f8baf4517715fca32dc362ba1a17c25e6
Andrey <and.veys@gmail.com>
2021-07-09 19:43:28 +0300

    test_test

--- hw.cpp
+++ hw.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <locale>				//для setlocale
 int main()
 {
-	setlocale(LC_CTYPE, "Russian");		//для вывода кирилицей  
 	std::cout << "Привет, Мир!" << std::endl;
 	return 0;
 }
