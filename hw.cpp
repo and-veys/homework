d41249d5008f2ecbf2a5773d3d8c148406c5ab0a
veys <and.veys@gmail.com>
2021-08-03 11:03:34 +0300

    Homework_6

--- hw.cpp
+++ hw.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
-#include <locale>				//для setlocale
+std::ostream& endll(std::ostream &out)
+{
+	out << "\n" << std::endl;
+	return out;
+}
+bool getInteger(int & nValue)
+{
+	nValue = 0;
+	std::string buffer;
+	std::cout << "Enter a number: ";		
+	std::getline(std::cin, buffer);	
+	if(buffer.size() == 0) return false;
+	int nSign = 1;
+	int i=0;
+	if(buffer.size() != 1 && buffer[0] == '-')
+	{
+		++i;
+		nSign = -1;
+	}
+	for(i; i<buffer.size(); ++i)
+	{
+		if(buffer[i] >= '0' && buffer[i] <= '9')
+			nValue = nValue*10 + buffer[i] - '0';
+		else return false;		
+	}
+	nValue*=nSign;
+	return true;	
+}
 int main()
 {
-	setlocale(LC_CTYPE, "Russian");		//для вывода кирилицей  
-	std::cout << "Привет, Мир!" << std::endl;
-	return 0;
+	std::cout << "_____Exercise #1_____" << std::endl;
+	int nValue;
+	while(!getInteger(nValue)) 
+		std::cout << "Error!! ";
+	std::cout << "Number is " << nValue << std::endl;
+	
+	std::cout << "_____Exercise #2_____" << std::endl;	
+	std::cout << "Hello world" << endll << "!!!" << std::endl;
+    return 0;
 }
+
+
